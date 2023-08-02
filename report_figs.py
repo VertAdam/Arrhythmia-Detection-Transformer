559d26b6301cb93752fc957b97a58954600ed50c
VertAdam <vertadam@gmail.com>
2023-08-02 17:11:19 -0400

    Added combined confusion matrix

--- report_figs.py
+++ report_figs.py
@@ -6,6 +6,7 @@ from main import get_predictions_and_labels
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from matplotlib.gridspec import GridSpec
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -32,11 +33,10 @@ cm = confusion_matrix(y_true, y_pred)
 row_sums = cm.sum(axis=1)
 col_sums = cm.sum(axis=0)
 
-# append sums to labels
 labels_row_sums = [f'{label} ({sum})' for label, sum in zip(classes, row_sums)]
 labels_col_sums = [f'{label} ({sum})' for label, sum in zip(classes, col_sums)]
 cmNorm = confusion_matrix(y_true, y_pred, normalize = 'true')
-# plot with column sums
+
 disp = ConfusionMatrixDisplay(confusion_matrix=cmNorm, display_labels=labels_row_sums)
 disp.plot(cmap='Blues')
 new_locations = np.arange(len(labels_col_sums))
@@ -70,7 +70,6 @@ cm = confusion_matrix(y_true, y_pred)
 row_sums = cm.sum(axis=1)
 col_sums = cm.sum(axis=0)
 
-# append sums to labels
 labels_row_sums = [f'{label} ({sum})' for label, sum in zip(classes, row_sums)]
 labels_col_sums = [f'{label} ({sum})' for label, sum in zip(classes, col_sums)]
 cmNorm = confusion_matrix(y_true, y_pred, normalize = 'true')
@@ -82,3 +81,39 @@ plt.xticks(labels=labels_col_sums, ticks = new_locations)
 plt.suptitle("Cross Entropy Loss")
 plt.savefig(f'plots/report_figs/confusion_matrix_CrossEntropy_normed.png')
 plt.close()
+
+
+
+
+fig = plt.figure(figsize=(20,10))
+gs = GridSpec(1, 2, figure=fig)
+
+ax1 = fig.add_subplot(gs[0, 0])
+
+model.load_state_dict(torch.load('models/ResNetTransformer_best.pt'))
+
+y_pred, y_true = get_predictions_and_labels(test_loader, model, device)
+
+cm = confusion_matrix(y_true, y_pred)
+cmNorm = confusion_matrix(y_true, y_pred, normalize = 'true')
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cmNorm, display_labels=labels_row_sums)
+disp.plot(cmap='Blues', ax=ax1)
+ax1.set_xticks(labels=labels_col_sums, ticks = new_locations)
+ax1.set_title("A. Class Balanced Focal Loss")
+
+ax2 = fig.add_subplot(gs[0, 1])
+
+model.load_state_dict(torch.load('models/ResNetTransformer_best_CrossEntropy.pt'))
+
+y_pred, y_true = get_predictions_and_labels(test_loader, model, device)
+
+cm = confusion_matrix(y_true, y_pred)
+cmNorm = confusion_matrix(y_true, y_pred, normalize = 'true')
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cmNorm, display_labels=labels_row_sums)
+disp.plot(cmap='Blues', ax=ax2)
+ax2.set_xticks(labels=labels_col_sums, ticks = new_locations)
+ax2.set_title("B. Cross Entropy Loss")
+plt.savefig('plots/report_figs/confusion_matrices_normed_combined.png')
+x = 1
